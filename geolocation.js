b6cd08dfbbc09d83f7db96f39f211c740aab776b
Manuel Bieh <github@manuelbieh.de>
2011-09-11 00:38:49 -0700

    Not augmenting the global object any longer. I hope it's more efficient now. fixed #1

--- geolocation.js
+++ geolocation.js
@@ -8,38 +8,38 @@
  * 
  * @author Manuel Bieh
  * @url http://www.manuel-bieh.de/
- * @version 1.0.9
+ * @version 1.0.10
  * @license http://www.gnu.org/licenses/lgpl-3.0.txt LGPL
  *
- * Date $LastChangedDate: 2011-03-29 19:47:37 +0200 (Di, 29 Mrz 2011) $
+ * Date $LastChangedDate: 2011-09-11 09:36:49 +0200 (Su, 11 Sep 2011) $
  *
  */
 
 
 ;(function() {
 
-	var geolocation = this;
+	var	geolocation = {};
 
-	this.init = function() {
+	geolocation.init = function() {
 
 		try {
 
 			// Check for W3C Geolocation API standard support
 			if(typeof (navigator.geolocation) != 'undefined') {
 
-				geolocation.type = 'W3C Geolocation API';
+				//geolocation.type = 'W3C Geolocation API';
 				geolocation.api = navigator.geolocation;
 
 			// Check for Google Gears support. gears_init.js must be included!
 			} else if(typeof (window.google) != 'undefined' &&  typeof(window.google.gears) != 'undefined') {
 
-				geolocation.type = 'Google Gears';
+				//geolocation.type = 'Google Gears';
 				geolocation.api = google.gears.factory.create('beta.geolocation');
 
 			// Checks for native Blackberry support
 			} else if (typeof(window.blackberry) != 'undefined' && blackberry.location.GPSSupported) {
 
-				geolocation.type = 'Blackberry OS';
+				//geolocation.type = 'Blackberry OS';
 				geolocation.api = new BlackberryLocation();
 
 			} else {
@@ -49,7 +49,7 @@
 			}
 
 			window.navigator.geolocation = geolocation.api;
-			window.navigator.geolocation['type'] = geolocation.type;
+			//window.navigator.geolocation['type'] = geolocation.type;
 
 			return true;
 
@@ -71,7 +71,7 @@
 	 * @param function Options
 	 * @return void
 	 */
-	this.getCurrentPosition = function(successCallback, errorCallback, options) {
+	geolocation.getCurrentPosition = function(successCallback, errorCallback, options) {
 
 		if(geolocation.api) {
 			geolocation.api.getCurrentPosition(successCallback, errorCallback, options);
@@ -87,7 +87,7 @@
 	 * @param function Options
 	 * @return integer ID of the watchPosition callback
 	 */
-	this.watchPosition = function(successCallback, errorCallback, options) {
+	geolocation.watchPosition = function(successCallback, errorCallback, options) {
 
 		if(geolocation.api) {
 			geolocation.watchID = geolocation.api.watchPosition(successCallback, errorCallback, options);
@@ -103,7 +103,7 @@
 	 * @param integer ID of the watchPosition 
 	 * @return void
 	 */
-	this.clearWatch = function(watchID) {
+	geolocation.clearWatch = function(watchID) {
 
 		if(watchID == NULL) {
 			geolocation.api.clearWatch(geolocation.watchID);
@@ -113,7 +113,7 @@
 
 	}
 
-	this.init();
+	geolocation.init();
 
 })();
 
@@ -126,6 +126,8 @@
 function BlackberryLocation() {
 
 	bb = this;
+	console.log(this);
+	return;
 
 	this.getCurrentPosition = function(successCallback, errorCallback, options) {
 
